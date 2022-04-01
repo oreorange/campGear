0f84900adc19533c03751803c2360cc7d0e7c5e4
oreorange <92431652+oreorange@users.noreply.github.com>
2022-04-01 14:52:29 +0000

    [Fix]table view

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_03_27_224039) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
